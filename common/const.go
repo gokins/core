abcf6800e8a21bd970f65293b24dc5324620d7b8
LinskRuis.32G <mgr9525@qq.com>
2021-06-25 12:04:16 +0800

    login api

--- common/const.go
+++ common/const.go
@@ -2,6 +2,14 @@ package common
 
 import "regexp"
 
+const (
+	TimeFmt    = "2006-01-02 15:04:05"
+	TimeFmts   = "2006-01-02"
+	TimeFmtm   = "2006-01"
+	TimeFmtpck = "2006-01-02T15:04:05.999999999Z"
+	TimeFmtVer = "20060102150405.999999999"
+)
+
 var (
 	RegHost1 = regexp.MustCompile(`^([\w\.]+)?:(\d+)?$`)
 	RegHost2 = regexp.MustCompile(`^([\w\.]+)(:\d+)?$`)
