abe943553934afdbf198fe4be31af593bc278bf9
LinsRuis.wubt20 <mgr9525@qq.com>
2022-08-02 09:36:57 +0800

    RepSeparators

--- utils/io.go
+++ utils/io.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"strings"
 )
 
 func Md5String(data string) string {
@@ -10,4 +11,8 @@ func Md5String(data string) string {
 	m5.Write([]byte(data))
 	md5Data := m5.Sum([]byte(nil))
 	return hex.EncodeToString(md5Data)
+}
+
+func RepSeparators(pth string) string {
+	return strings.ReplaceAll(pth, "\\", "/")
 }
